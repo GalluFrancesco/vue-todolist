8da28a4420faf64fed7320e582bc9875c5d27994
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-01-17 15:14:26 +0100

    Add: lista oggetti todo & stamp lista html

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 new Vue({
     el:"#app",
     data:{
-
+        todoList:[
+            {
+            txt:"Commissione 1",
+            done:false
+            },
+            { 
+            txt:"Commissione 2",
+            done:true
+            },
+            { 
+            txt:"Commissione 3",
+            done:false
+            },
+            { 
+            txt:"Commissione 4",
+            done:false
+            }
+        ]
     }
 })
